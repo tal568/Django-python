27cdabd72b229bf1d13f13f7d1728380c1012ff3
tal_l <tal9lev@gmail.com>
2024-07-19 18:15:32 +0300

    fix: :bug: making discount optionable

--- elevate/api/models.py
+++ elevate/api/models.py
@@ -6,7 +6,7 @@ class product(models.Model):
     name = models.CharField(max_length=200)
     description = models.CharField(max_length=600)
     price = models.DecimalField(max_digits=10,decimal_places=2)
-    discounted_price = models.DecimalField(max_digits=10,decimal_places=2,null=True)
+    discounted_price = models.DecimalField(max_digits=10,decimal_places=2,null=True,blank=True)
 
 
     def __str__(self):
