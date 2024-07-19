2f76ab6af6eb22f5f580be1b740cd763a67b6912
tal_l <tal9lev@gmail.com>
2024-07-19 17:11:55 +0300

    feat: :triangular_flag_on_post: adding product model

--- elevate/api/models.py
+++ elevate/api/models.py
@@ -6,6 +6,8 @@ class product(models.Model):
     name = models.CharField(max_length=200)
     description = models.CharField(max_length=600)
     price = models.DecimalField(max_digits=10,decimal_places=2)
+    discounted_price = models.DecimalField(max_digits=10,decimal_places=2,null=True)
+
 
     def __str__(self):
         return self.name
