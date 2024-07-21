2bb35f502f4ccad8e7775ffc495fe3f960e44cd6
tal_l <tal9lev@gmail.com>
2024-07-21 18:46:30 +0300

    feat: :sparkles: update a product

--- elevate/api/views.py
+++ elevate/api/views.py
@@ -36,5 +36,12 @@ def product(request,productId):
     if request.method == 'DELETE':
         count=get_object_or_404(Product,id=productId).delete()
         return Response({'deleted':count[0]})
-
+    if request.method == 'PUT':
+        product=get_object_or_404(Product,id=productId)
+        
+        serializer=productSerializer(product,data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors,status=HTTP_400_BAD_REQUEST)
     
